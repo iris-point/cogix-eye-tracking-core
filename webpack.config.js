5a6c21a8b119fee15c9a2b84191da745c03210c3
Clay <radiantclay@gmail.com>
2025-09-02 14:08:04 -0400

    fix: remove copy-webpack-plugin dependency and use standalone build script for plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
 const path = require('path');
-const TerserPlugin = require('terser-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
@@ -53,69 +51,6 @@ module.exports = (env, argv) => {
         extensions: ['.js']
       },
       devtool: isProduction ? 'source-map' : 'inline-source-map'
-    },
-    // Copy and minify plugin/extension files directly (they're already IIFE format)
-    {
-      entry: './src/dummy.js', // Dummy entry since we're just copying
-      output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'dummy.js' // Will be deleted
-      },
-      plugins: [
-        new CopyWebpackPlugin({
-          patterns: [
-            // Copy and optionally minify plugin files
-            {
-              from: 'jspsych-plugin/plugin-cogix-init-camera.js',
-              to: isProduction ? 'plugin-cogix-init-camera.min.js' : 'plugin-cogix-init-camera.js',
-              transform: isProduction ? undefined : (content) => content,
-              transformPath: (targetPath) => targetPath
-            },
-            {
-              from: 'jspsych-plugin/plugin-cogix-calibrate.js',
-              to: isProduction ? 'plugin-cogix-calibrate.min.js' : 'plugin-cogix-calibrate.js',
-              transform: isProduction ? undefined : (content) => content,
-              transformPath: (targetPath) => targetPath
-            },
-            {
-              from: 'jspsych-plugin/plugin-cogix-validate.js',
-              to: isProduction ? 'plugin-cogix-validate.min.js' : 'plugin-cogix-validate.js',
-              transform: isProduction ? undefined : (content) => content,
-              transformPath: (targetPath) => targetPath
-            },
-            // Also copy non-minified versions in production
-            ...(isProduction ? [
-              {
-                from: 'jspsych-plugin/plugin-cogix-init-camera.js',
-                to: 'plugin-cogix-init-camera.js'
-              },
-              {
-                from: 'jspsych-plugin/plugin-cogix-calibrate.js',
-                to: 'plugin-cogix-calibrate.js'
-              },
-              {
-                from: 'jspsych-plugin/plugin-cogix-validate.js',
-                to: 'plugin-cogix-validate.js'
-              }
-            ] : [])
-          ]
-        })
-      ],
-      optimization: {
-        minimize: isProduction,
-        minimizer: isProduction ? [
-          new TerserPlugin({
-            test: /\.min\.js$/,
-            terserOptions: {
-              compress: true,
-              mangle: true,
-              format: {
-                comments: false
-              }
-            }
-          })
-        ] : []
-      }
     }
   ];
 };
